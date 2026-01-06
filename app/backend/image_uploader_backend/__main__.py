bb1c1d06ccbfd5464dfcc194a85171955bd839e6
Maker By Night <2565361+mshafer1@users.noreply.github.com>
2026-01-05 19:24:47 -0600

    move to .github and use ni/python-renovate-config as base

--- app/backend/image_uploader_backend/__main__.py
+++ app/backend/image_uploader_backend/__main__.py
@@ -2,5 +2,4 @@ import image_uploader_backend
 import logging
 
 if __name__ == '__main__':
-    # host="0.0.0.0"
-    image_uploader_backend.app.run()
+    image_uploader_backend.app.run(host="0.0.0.0")
